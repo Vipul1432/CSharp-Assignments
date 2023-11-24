451d64b7a9572fd231deff9ff2961d907240c8c0
Vipul143212 <vipulu563@gmail.com>
2023-11-24 10:28:18 +0530

    Add assignment-1

---
+++ Assignment-1/Employee.cs
@@ -0,0 +1,95 @@
+﻿namespace Assignment_1
+{
+    public class Employee
+    {
+        private int Id;
+        private string Name;
+        private string DepartmentName;
+
+        // Event declaration
+        public event EventHandler MethodCalled;
+
+        /// <summary>
+        /// Initializes a new instance of the Employee class with specified values.
+        /// </summary>
+        /// <param name="id">The ID of the employee.</param>
+        /// <param name="name">The name of the employee.</param>
+        /// <param name="departmentName">The department name of the employee.</param>
+        public Employee(int id, string name, string departmentName)
+        {
+            Id = id;
+            Name = name;
+            DepartmentName = departmentName;
+        }
+
+        /// <summary>
+        /// Retrieves the ID of the employee.
+        /// </summary>
+        /// <returns>The ID of the employee.</returns>
+        public int GetId()
+        {
+            OnMethodCalled("GetId() method called");
+            return Id;
+        }
+
+        /// <summary>
+        /// Retrieves the name of the employee.
+        /// </summary>
+        /// <returns>The name of the employee.</returns>
+        public string GetName()
+        {
+            OnMethodCalled("GetName() method called");
+            return Name;
+        }
+
+        /// <summary>
+        /// Retrieves the department name of the employee.
+        /// </summary>
+        /// <returns>The department name of the employee.</returns>
+        public string GetDepartmentName()
+        {
+            OnMethodCalled("GetDepartmentName() method called");
+            return DepartmentName;
+        }
+
+        /// <summary>
+        /// Updates the ID of the employee with the specified value.
+        /// </summary>
+        /// <param name="newId">The new ID to set for the employee.</param>
+        public void UpdateId(int newId)
+        {
+            Id = newId;
+            OnMethodCalled("UpdateId() method called");
+        }
+
+        /// <summary>
+        /// Updates the name of the employee with the specified value.
+        /// </summary>
+        /// <param name="newName">The new name to set for the employee.</param>
+        public void UpdateName(string newName)
+        {
+            Name = newName;
+            OnMethodCalled("UpdateName() method called");
+        }
+
+        /// <summary>
+        /// Updates the department name of the employee with the specified value.
+        /// </summary>
+        /// <param name="newDepartmentName">The new department name to set for the employee.</param>
+        public void UpdateDepartmentName(string newDepartmentName)
+        {
+            DepartmentName = newDepartmentName;
+            OnMethodCalled("UpdateDepartmentName() method called");
+        }
+
+        /// <summary>
+        /// Invokes the MethodCalled event and prints a message indicating the method called.
+        /// </summary>
+        /// <param name="message">The message to print indicating the method called.</param>
+        protected virtual void OnMethodCalled(string message)
+        {
+            MethodCalled?.Invoke(this, new EventArgs());
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Assignment-1/Program.cs
@@ -0,0 +1,127 @@
+﻿namespace Assignment_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Employee Details:");
+                Console.Write("Id: ");
+                int id = ReadIntFromConsole();
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Department Name: ");
+                string departmentName = Console.ReadLine();
+
+                // Creating an object of the Employee class
+                Employee employee = new Employee(id, name, departmentName);
+
+                // Subscribing to the event
+                employee.MethodCalled += Employee_MethodCalled;
+
+                // Printing details using getter methods
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nEmployee Details:");
+                Console.WriteLine($"ID: {employee.GetId()}");
+                Console.WriteLine($"Name: {employee.GetName()}");
+                Console.WriteLine($"Department Name: {employee.GetDepartmentName()}");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                // Ask the user if they want to update any property in a loop
+                bool updateOption = true;
+                while (updateOption)
+                {
+                    Console.Write("\nDo you want to update any property? (Y/N): ");
+                    if (Console.ReadLine().ToUpper() == "Y")
+                    {
+                        // Ask which property to update
+                        Console.Write("Enter property to update (Id, Name, or DepartmentName): ");
+                        string propertyToUpdate = Console.ReadLine();
+
+                        // Update the chosen property
+                        if (propertyToUpdate.Equals("Id", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.Write("Enter new Id: ");
+                            int newId = ReadIntFromConsole();
+                            employee.UpdateId(newId);
+                        }
+                        else if (propertyToUpdate.Equals("Name", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.Write("Enter new Name: ");
+                            string newName = Console.ReadLine();
+                            employee.UpdateName(newName);
+                        }
+                        else if (propertyToUpdate.Equals("DepartmentName", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.Write("Enter new Department Name: ");
+                            string newDepartmentName = Console.ReadLine();
+                            employee.UpdateDepartmentName(newDepartmentName);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid property name.");
+                        }
+
+                        // Print updated details using getter methods
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("\nUpdated Employee Details:");
+                        Console.WriteLine($"ID: {employee.GetId()}");
+                        Console.WriteLine($"Name: {employee.GetName()}");
+                        Console.WriteLine($"Department Name: {employee.GetDepartmentName()}");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        // Exit the loop if the user chooses not to update
+                        updateOption = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        /// <summary>
+        /// Reads an integer input from the console, continuously prompting the user until a valid integer is entered.
+        /// If the input is not a valid integer, an error message is displayed in red.
+        /// </summary>
+        /// <returns>The valid integer entered by the user.</returns>
+        private static int ReadIntFromConsole()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int result))
+                {
+                    return result;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+        }
+        /// <summary>
+        /// Event handler method for the MethodCalled event in the Employee class.
+        /// Displays a message in blue indicating that a method has been called in the Employee class.
+        /// </summary>
+        /// <param name="sender">The object that triggered the event.</param>
+        /// <param name="e">The event arguments.</param>
+
+        private static void Employee_MethodCalled(object sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Event: Method called in Employee class");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
