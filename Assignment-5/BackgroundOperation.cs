72f3d665632e411f8c0bfb4945dcc717fd75d720
Vipul143212 <vipulu563@gmail.com>
2023-11-27 10:17:15 +0530

    Add Assignment-5

---
+++ Assignment-5/BackgroundOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    public class BackgroundOperation
+    {
+        /// <summary>
+        /// Writes a message to a file asynchronously with a simulated delay.
+        /// </summary>
+        /// <param name="message">The message to be written to the file.</param>
+        /// <returns>An asynchronous task representing the file writing operation.</returns>
+        public async Task WriteToFileAsync(string message)
+        {
+            try
+            {
+                Console.WriteLine("Writing to file...");
+                await Task.Delay(3000);
+                string filePath = Path.Combine("C:", "Users", "admin", "source", "repos", "Assignment-1", "Assignment-5", "tmp.txt");
+                await File.WriteAllTextAsync(filePath, message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred during file writing: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Assignment-5/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Assignment_5
+{
+    public class Program
+    {
+        private static BackgroundOperation backgroundOperation = new BackgroundOperation();
+
+        public static async Task Main()
+        {
+            try
+            {
+                ShowMenu();
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int choice))
+                {
+                    await ExecuteNonBlockingOperation(choice);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Displays the menu options for the Kiosk system.
+        /// </summary>
+        private static void ShowMenu()
+        {
+            Console.WriteLine("Kiosk Menu:");
+            Console.WriteLine("1. Write \"Hello World\"");
+            Console.WriteLine("2. Write Current Date");
+            Console.WriteLine("3. Write OS Version");
+            Console.Write("Enter your choice (1, 2, or 3): ");
+        }
+
+        /// <summary>
+        /// Executes the chosen non-blocking operation based on user input. Retrieves the appropriate message,
+        /// performs the non-blocking write operation, and handles exceptions if they occur.
+        /// </summary>
+        /// <param name="choice">The user's menu choice.</param>
+        private static async Task ExecuteNonBlockingOperation(int choice)
+        {
+            string message = GetMessageBasedOnChoice(choice);
+
+            try
+            {
+                Console.WriteLine("Performing non-blocking operation...");
+                await backgroundOperation.WriteToFileAsync(message);
+                Console.WriteLine("Operation completed successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred during the operation: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Returns a message based on the user's menu choice. 
+        /// </summary>
+        /// <param name="choice">The user's menu choice.</param>
+        /// <returns>A message corresponding to the user's choice.</returns>
+        private static string GetMessageBasedOnChoice(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    return "Hello World";
+                case 2:
+                    return DateTime.Now.ToString();
+                case 3:
+                    return Environment.OSVersion.VersionString;
+                default:
+                    throw new ArgumentException("Invalid choice.");
+            }
+        }
+    }
+}
